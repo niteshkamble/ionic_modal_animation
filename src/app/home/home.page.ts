5cce7ef5688579e3b398048f11631afafb68d86e
NiteshKamble <kamblenitesh351@gmail.com>
2023-02-20 02:07:50 +0530

    ion modal enter and leaving animation added

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { ActionSheetController, AnimationController, ModalController } from '@ionic/angular';
 
 @Component({
   selector: 'app-home',
@@ -7,6 +8,78 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
-  constructor() {}
+  constructor(private  actionSheetCtrl:ActionSheetController,private animationCtrl:AnimationController) {}
+
+
+  async openModal(){
+    const actionSheet = await this.actionSheetCtrl.create({
+      header: 'Example header',
+      subHeader: 'Example subheader',
+      enterAnimation: (baseEl : any)=>{
+        const root = baseEl;
+
+        const backDropAnimation = this.animationCtrl.create()
+          .addElement(root.querySelector('ion-backdrop')!)
+          .fromTo('opacity','0.01','var(--backdrop-opacity)');
+
+        const wrapperAnimation = this.animationCtrl.create()
+          .addElement(root.querySelector('.action-sheet-wrapper')!)
+          .keyframes([
+            {offset:0,transform:'translateX(-100%)'},
+            {offset:1,transform:'translateX(0)'}
+          ])
+
+          return this.animationCtrl.create()
+          .addElement(baseEl)
+          .easing('ease-out')
+          .duration(500)
+          .addAnimation([backDropAnimation,wrapperAnimation]);
+      },
+      leaveAnimation : (baseEl : any)=>{
+        const root = baseEl;
+
+        const backDropAnimation = this.animationCtrl.create()
+          .addElement(root.querySelector('ion-backdrop')!)
+          .fromTo('opacity','var(--backdrop-opacity)','0');
+
+        const wrapperAnimation = this.animationCtrl.create()
+          .addElement(root.querySelector('.action-sheet-wrapper')!)
+          .keyframes([
+            {offset:0,transform:'translateX(0)'},
+            {offset:1,transform:'translateX(100%)'},
+          ])
+
+          return this.animationCtrl.create()
+          .addElement(baseEl)
+          .easing('ease-out')
+          .duration(500)
+          .addAnimation([backDropAnimation,wrapperAnimation]);
+      },
+      buttons: [
+        {
+          text: 'Delete',
+          role: 'destructive',
+          data: {
+            action: 'delete',
+          },
+        },
+        {
+          text: 'Share',
+          data: {
+            action: 'share',
+          },
+        },
+        {
+          text: 'Cancel',
+          role: 'cancel',
+          data: {
+            action: 'cancel',
+          },
+        },
+      ],
+    });
+
+    await actionSheet.present();
+  }
 
 }
